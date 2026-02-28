f511b67a96edb391eb04636faa5ac96e06d265c6
hibawahab356 <ranihiba356@gmail.com>
2026-02-28 19:24:15 +0500

    Chapter 4 assignment

--- Chapter 4/app.js
+++ Chapter 4/app.js
@@ -3,11 +3,38 @@
 // document.write("My name is " + name + " I am " + age + " years old and I live in " + country)
 
 
+
+// question : 02
+
+// legal variables names
+let institute
+let instituteName
+let institute_2025
+let $instituteFee
+let _instituteCode
+
+
+// illegal variables names
+
+
+
+
+
 // question : 03
 
 // A
-// document.write("<h1> Rules for naming JS variables <h1>")
+document.write("Rules for naming JS variables <br> ")
+
+// B
+document.write("Variable names can only contain $my, _ , instituteName <br>")
+
+// C
+document.write("Variables must begin with a $name, _name or instituteName <br>")
 
 // D
-// let $myVariable = "sensitive"
-// document.write("<h3>Variable names are case + $myVariable </h3>" ) 
+let thisCase = "sensitive"
+document.write("Variable names are case " +  thisCase  )
+
+
+// E
+document.write("Variable names should not be JS keywords <br>")
