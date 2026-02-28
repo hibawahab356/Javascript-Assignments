d0aafe1b67b4038f6bf579f8afcf8240149857f2
hibawahab356 <ranihiba356@gmail.com>
2026-02-28 13:02:14 +0500

    js first chapter assignment

---
+++ Chapter 1/app.js
@@ -0,0 +1,28 @@
+// question: 01
+// alert("Welcome to my website")
+
+
+
+// question : 02
+// alert("Error!Please enter a valid password")
+
+
+
+// question : 03
+// alert("Welcome to JS Land...\nHappy Coding!")
+
+
+
+// question : 04 
+// alert("Welcome to JS Land...")
+// alert("Happy Coding")
+
+
+
+// question : 05
+// alert("Hello... I can run JS through my browser's console")
+
+
+
+
+
