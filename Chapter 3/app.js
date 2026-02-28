ceb00dddaba044494325fbecd106c3f1386c7043
hibawahab356 <ranihiba356@gmail.com>
2026-02-28 13:34:51 +0500

    chapter 3 assignment

---
+++ Chapter 3/app.js
@@ -0,0 +1,25 @@
+// question : 01
+// let age = 17
+// alert("I am " + age + " years old")
+
+
+// question : 02
+// let visit = 18
+// alert("You have visited this site " + visit + " times")
+
+
+// question : 03
+// let birthyear = 2008
+// document.write("My birth year is " + birthyear + "<br> Data type of my declared variable is number")
+
+
+// question : 04
+// let visitorName = "John Doe"
+// let product =  "T-shirt"
+// let quantity = 10
+
+// document.write(visitorName + " ordered " + quantity + " " + product + " on xyz clothing store")
+
+
+
+
