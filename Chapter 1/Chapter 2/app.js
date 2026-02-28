413d98f754b442fe4d320bd33fde5fb36a64d1e2
hibawahab356 <ranihiba356@gmail.com>
2026-02-28 13:04:39 +0500

    js sceond chapter assignment

---
+++ Chapter 1/Chapter 2/app.js
@@ -0,0 +1,47 @@
+// question : 01
+// let userName 
+// console.log(userName)
+
+
+// question : 02
+// let myName = "Hiba"
+// console.log(myName)
+
+
+// question : 03
+// let message = "Hello World!"
+// alert(message)
+
+
+// question : 04
+// let studentName = "Jhon Doe"
+// let studentAge = 15 + " years old"
+// let studentCourse = "Certified Mobile Application Development"
+
+// alert(studentName)
+// alert(studentAge)
+// alert(studentCourse)
+
+
+// question : 05
+// let food = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+// alert(food)
+
+
+// question : 06
+// let email = "hiba@example.com";
+// alert("My email address is " + email);
+
+
+// question : 07
+// let book = "A smarter way to learn JavaScript"
+// alert("I am trying to learn from the book " + book)
+
+
+// question : 08
+// document.write("Yah! I can write HTML content through JavaScript")
+
+
+// question : 09
+// let pattern = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(pattern)
