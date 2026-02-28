ff056f34d0e55399d8a83070f60a4cc3134c4783
hibawahab356 <ranihiba356@gmail.com>
2026-02-28 13:58:45 +0500

    chapter 4 assignment

---
+++ Chapter 4/app.js
@@ -0,0 +1,13 @@
+// question : 01
+// let name = "John Doe", age = 25, country = "USA" 
+// document.write("My name is " + name + " I am " + age + " years old and I live in " + country)
+
+
+// question : 03
+
+// A
+// document.write("<h1> Rules for naming JS variables <h1>")
+
+// D
+// let $myVariable = "sensitive"
+// document.write("<h3>Variable names are case + $myVariable </h3>" ) 
