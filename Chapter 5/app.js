61d04ebc35644dffe7ee131af87f86566016cd6b
hibawahab356 <ranihiba356@gmail.com>
2026-03-07 13:55:10 +0500

    Chapter 5

---
+++ Chapter 5/app.js
@@ -0,0 +1,58 @@
+// question : 01
+// let num1 = +prompt("Enter your First Number")
+// let num2 = +prompt("Enter your Second Number")
+
+// let sumTotal = num1 + num2
+
+// document.write(`Sum of ${num1} and ${num2} is ${sumTotal}`)
+
+
+
+// question : 02
+
+// a
+
+// let num3 = prompt("Enter your First Number")
+// let num4 = prompt("Enter your Second Number")
+
+// let subTotal = num3 - num4
+
+// document.write(`Subtraction of ${num3} and ${num4} is ${subTotal}`)
+
+
+// b
+
+// let num5 = prompt("Enter your First Number")
+// let num6 = prompt("Enter your Second Number")
+// let multiTotal = num5 * num6 
+
+// document.write(`Multiplication of ${num5} and ${num6} is ${multiTotal}`)
+
+// c
+
+// let num7 = prompt("Enter your First Number")
+// let num8 = prompt("Enter your Second Number")
+// let divideTotal = num7 / num8
+
+// document.write(`Dividation of ${num7} and ${num8} is ${divideTotal}`)
+
+
+
+// question : 02
+
+// a
+let num
+
+// b
+
+document.write(`Value after variable declaration is`)
+
+// c
+num = 7
+
+// d
+document.write(`${num}`)
+
+// e
+
+document.write(`${++num}`)
