06fb7f964f27a97ba5985d1f45cff4ee4f797b1e
hibawahab356 <ranihiba356@gmail.com>
2026-04-03 14:52:30 +0500

    chap 9to 11

---
+++ chap 9to 11/app.js
@@ -0,0 +1,300 @@
+
+// question : 01
+// let a = 10
+
+// document.write(`The value of a is ${a}`)
+// document.write(`<br />`)
+
+
+// document.write(`..........................`)
+// document.write(`<br />`)
+// document.write(`<br />`)
+
+
+// ++a Preincrement
+
+// document.write(`The value of ++a is${++a}`)
+// document.write(`<br />`)
+
+// document.write(`Now the value of a is ${a}`)
+// document.write(`<br />`)
+// document.write(`<br />`)
+
+// a++ Postdecrement
+
+
+// document.write(`The value of a++ is${a++}`)
+// document.write(`<br />`)
+
+// document.write(`Now the value of a is ${a}`)
+// document.write(`<br />`)
+// document.write(`<br />`)
+
+
+// Postincrement
+
+// document.write(`The value of --a is ${--a}`)
+// document.write(`<br />`)
+
+// document.write(`Now the value of a is ${a}`)
+// document.write(`<br />`)
+// document.write(`<br />`)
+
+
+// a++ Postdecrement
+
+
+// document.write(`The value of a-- is ${a--}`)
+// document.write(`<br />`)
+
+// document.write(`Now the value of a is ${a}`)
+// document.write(`<br />`)
+// document.write(`<br />`)
+
+
+
+// question : 02
+
+// let a = 2
+// let b = 1
+
+// --a
+// document.write(`The value of a is ${a}`)
+// document.write(`<br />`)
+
+// --a- --b
+
+// 0 - 0
+//   0
+
+// document.write(`The value of a is ${a} and ${b}`)
+// document.write(`<br />`)
+
+
+//   --a - --b + ++b; 
+//   -1 - -1 + 1 
+//   -1 - 0  
+//   a = -1 b = 0
+
+// document.write(`The value of a is ${a} and ${b}`)
+// document.write(`<br />`)
+
+
+//    --a - --b + ++b + b--; 
+
+// document.write(`The value of a is ${a} and ${b}`)
+// document.write(`<br />`)
+
+
+
+// question : 03
+
+// let userName = prompt("Whats your Name?")
+
+// let greet = alert(`Hello ${userName}`)
+
+
+// question : 04
+
+
+
+// let num = prompt("Type a number")
+
+
+// let tableName = num
+// let count = 1 
+
+// document.write(`${tableName} x ${count} = ${tableName}`)
+// document.write(`<br />`)
+
+
+// ++count 
+// document.write(`${tableName} x ${count} = ${tableName*count}`)
+// document.write(`<br />`)
+
+// ++count 
+// document.write(`${tableName} x ${count} = ${tableName*count}`)
+// document.write(`<br />`)
+
+// ++count 
+// document.write(`${tableName} x ${count} = ${tableName*count}`)
+// document.write(`<br />`)
+
+// ++count 
+// document.write(`${tableName} x ${count} = ${tableName*count}`)
+// document.write(`<br />`)
+
+// ++count 
+// document.write(`${tableName} x ${count} = ${tableName*count}`)
+// document.write(`<br />`)
+
+// ++count 
+// document.write(`${tableName} x ${count} = ${tableName*count}`)
+// document.write(`<br />`)
+
+// ++count 
+// document.write(`${tableName} x ${count} = ${tableName*count}`)
+// document.write(`<br />`)
+
+// ++count 
+// document.write(`${tableName} x ${count} = ${tableName*count}`)
+// document.write(`<br />`)
+
+// ++count 
+// document.write(`${tableName} x ${count} = ${tableName*count}`)
+// document.write(`<br />`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Chapter 9 to 11
+// question : 01
+// let cityName = prompt("Whats your city name?") 
+
+// if (cityName==="Karachi"){
+//     alert("Welcome to the city of lights")
+// }
+
+// else{
+//     alert("Welcome")
+// }
+
+
+// question :02
+// let gender = prompt("May I know your gender?")
+
+// if (gender==="Male"){
+//     alert("Good Morning Sir")
+// }
+
+// else{
+//     alert("Good Morning Ma’am")
+// }
+
+
+
+//question : 03
+// let colorSignal = prompt("What is the color of the traffic signal?")
+
+// document.write(`Red <br/> Must stop`)
+// document.write(`<br/>`)
+
+// document.write(`Yellow <br/> Ready to move`)
+// document.write(`<br/>`)
+
+// document.write(`Green <br/> Move now`)
+// document.write(`<br/>`)
+
+
+// question : 04
+
+// let feul = prompt("How much fuel is left in your car (in liters)?")
+
+// if (feul<=0.25){
+//     alert("Please refill the fuel in your car")
+// }
+
+// else{
+//     alert("Fuel level is sufficient. No need to refill")
+// }
+
+
+// question : 05
+
+// a
+
+// let a = 4 
+
+// if (++a === 5){ 
+// alert("given condition for variable a is true"); 
+// }
+
+// b
+
+// let b = 82; 
+// if (b++ === 83){ 
+//     alert("given condition for variable b is true")
+
+// } 
+
+
+// else{
+//    alert("given condition for variable b is false")
+// }
+
+// c
+
+// let  c = 12; 
+
+// if (c++ === 13){ 
+// alert("condition 1 is true"); 
+// } 
+
+// else{
+//    alert("given condition 1 is false")
+// }
+
+
+// if (c === 13){ 
+// alert("condition 2 is true"); 
+// } 
+
+// else{
+//    alert("given condition 2 is false")
+// }
+
+ 
+// if (++c < 14){ 
+// alert("condition 3 is true"); 
+// } 
+
+// else{
+//    alert("given condition 3 is false")
+// }
+
+// if(c === 14){ 
+// alert("condition 4 is true"); 
+// } 
+
+// else{
+//    alert("given condition 4 is false")
+// }
+
+
+// d 
+
+// let materialCost = 20000; 
+// let laborCost = 2000; 
+// let totalCost = materialCost + laborCost; 
+
+// if (totalCost === laborCost + materialCost){ 
+// alert("The cost equals"); 
+// } 
+
+// else{
+//     alert("The cost is not equal")
+// }
+
+
+// e
+
+// if("car" < "cat"){ 
+// alert("car is smaller than cat"); 
+// } 
+
+// else{
+//     alert("Car is larger than cat")
+// }
