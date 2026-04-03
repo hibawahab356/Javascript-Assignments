cfcb155a2743d83428dd5f824b189f49dbb4c33d
hibawahab356 <ranihiba356@gmail.com>
2026-04-03 22:15:08 +0500

    chap 12to13

---
+++ chap 12 to 13/app.js
@@ -0,0 +1,121 @@
+// // q : 07
+// let secretNum = 8
+
+// let userNum = +prompt("Guess the number (1–10)")
+
+
+// if(secretNum === userNum){
+//     alert("Bingo! Correct answer")
+// }
+// else if(userNum + 1 === secretNum){
+//     alert("Close enough to the correct answer")
+// } 
+
+// else{
+//     alert("Wrong guess / Try again")
+// }
+
+
+
+
+// q : 08
+
+// let num = +prompt("Write any number")
+
+// if(num %3 === 0){
+//     alert("The number is divisible by 3")
+// }
+// else{
+//     alert("The number is not divisible by 3")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ch 12 to 13
+// question : 02
+
+// let num1 = +prompt("Enter first number")
+// let num2 = +prompt("Enter second number")
+
+// if(num1 > num2 ){
+    // alert(`First number ${num1} is larger`)
+// }
+
+// else if(num2 > num1 ){
+//     alert(` Second number ${num2} is larger`)
+// }
+
+// else{
+//      alert(`${num1} and ${num2} Both numbers are equal`)
+// }
+
+
+// q : 03
+// let num1 = +prompt("Enter first number")
+
+
+// if(num1 > 0){
+//      alert(` ${num1} It is a positive number`)
+// }
+
+// else if(num1 < 0){
+//      alert(`${num1} It is a negative number`)
+// }
+
+// else {
+//      alert(`${num1} It is a zero`)
+// }
+
+
+// q : 04
+// let letter1 = prompt("Enter any letter")
+
+// let vowels = a,e,i,o,u
+// if(letter1 == vowels ){
+//      alert(` ${letter1} It is a vowel true`)
+// }
+
+// else{
+//     alert(` ${letter1} It is not a vowel false`)
+// }
+
+// q : 05
+
+
+// let userpass = prompt("Enter your password")
+// let password = "welcome";
+// if(!userpass){
+//     alert("Enter your password")
+// }
+// else if(userpass === password){
+//     alert("Correct! The password you entered matches the original passwordCorrect")
+// }
+
+// else {
+//  alert("Incorrect password")
+// }
+
+// q :06
+// let userhours = prompt("Enter the current hour (0-23"); 
+
+
+// let greeting = "Good day"
+// let hours = 15
+
+// if(userhours < hours){
+//  alert(`${greeting}`)
+// }
+// else{
+//     alert("Good Evening")
+// }
+
