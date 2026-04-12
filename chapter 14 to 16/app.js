6468a4b821c4ccdbd976893c2429a38be6f50d75
hibawahab356 <ranihiba356@gmail.com>
2026-04-12 14:22:52 +0500

    chapter 14 to 16

---
+++ chapter 14 to 16/app.js
@@ -0,0 +1,159 @@
+// question: 1
+
+
+// let num = []
+
+// console.log( num)
+
+
+// question: 2
+
+// let students = new Array();
+
+// console.log(students)
+
+// question: 3
+
+// let fruits = ["Apple ", "mango", "kiwi"]
+
+// console.log(fruits[1])
+
+// question: 4
+
+// let numbers = [1 , 2 , 3 , 4 , 5]
+// document.write(numbers)
+
+// question : 5
+
+// let booleannum = [true , false , true , true] 
+// document.write(booleannum)
+
+
+// question : 6
+// let mixarray = [1 , " UK " , true , 0 , " Algeria "]
+// document.write(mixarray)
+
+// question : 7
+// let qualification = [ " SSC ", " HSC ", " BCS ", " BS ", " BCOM ", " MS ", " M. Phil.", " PhD"]
+
+// document.write(qualification)
+
+
+// question : 9
+
+// // a
+// let colors = ["purple" , "violet" , "magenda" , "pink"]
+// document.write(`Orignal Colors ${colors} <br>`)
+
+// // b
+// let userColor = prompt("Which color do you want to add at the beginning") 
+
+// colors.unshift(userColor)
+// document.write(`Updated Color: ${colors} <br>`)
+
+// // c
+// let lastColor = prompt("Which color do you want to add at the end") 
+
+// colors.push(lastColor)
+// document.write(`Updated Color: ${colors} <br>`)
+
+
+// // d
+
+// colors.unshift("red" , "White")
+// document.write(`Orignal Colors ${colors} <br>`)
+
+
+// // e
+
+// colors.shift()
+// document.write(`Updated Colors after removing first color: ${colors} <br>`)
+
+
+// // f
+// colors.pop()
+// document.write(`Updated Colors after removing last color: ${colors} <br>`)
+
+// // g
+// let color = ["gray", "green", "black", "yellow", "brown"];
+// document.write("Original Array: " + colors + "<br><br>");
+
+
+// let index = +prompt("Enter index number where you want to delete color(s):");
+
+
+// let deleteCount = +prompt("How many colors do you want to delete?");
+
+
+// colors.splice(index, deleteCount);
+
+// document.write("Updated Array: " + colors);
+
+
+
+// question 10
+
+// let score = [120,320,540]
+
+// console.log (score [2])
+
+
+
+// question 11
+
+// let cities = ["Karachi", "Hyderabad", "Faislabad", "Quetta", "Peshawar"];
+
+// let selectedCities = cities.slice(0, 3);
+
+
+// document.write("Cities List: " + cities + "<br><br>");
+// document.write("Selected Cities List: " + selectedCities);
+
+
+
+
+// question 12
+
+// let arr = ["This","is", "my", "cat"]; 
+
+// console.log(arr.join(" "))
+
+
+// question 13
+
+
+// let device = ["Keyboard", "mouse", "pointer", "monitor"]
+
+// console.log(device)
+// console.log(device.shift())
+
+// console.log(device)
+
+// console.log(device.shift())
+
+// console.log(device)
+
+// console.log(device.shift())
+
+// console.log(device)
+
+// console.log(device.shift())
+
+// console.log(device)
+
+
+// question: 14
+
+// let country = ["pakistan", "china", "usa"]
+
+// console.log(country.pop())
+
+// console.log(country)
+
+// console.log(country.pop())
+
+// console.log(country)
+
+// console.log(country.pop())
+
+// console.log(country)
