ce7b5163bb7b1811adc609464a5ed3cca9686ef1
hibawahab356 <alishu432@gmail.com>
2026-04-17 22:31:30 +0500

    chap17to20

---
+++ chap 17to 20/app.js
@@ -0,0 +1,171 @@
+// question 1
+
+// let namne = [[]];
+
+// document.write(name)
+
+
+
+
+
+
+
+
+
+
+// question 2 
+
+
+
+// let arr = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9]
+// ];
+
+// console.log(arr)
+
+
+
+// question 3
+
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// } 
+
+
+
+
+// question 4
+
+
+// let num = prompt("Enter table number:");
+// let length = prompt("Enter table length:");
+
+// for (let i = 1; i <= length; i++) {
+//   console.log(num + " x " + i + " = " + (num * i));
+// }
+
+
+
+// question 5
+
+
+// let fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+
+// for (let i = 0; i < fruits.length; i++) {
+//   document.write(fruits[i] + "<br>");
+// }
+
+
+
+// question 6
+
+// (a)
+
+// for (let i = 1; i <= 15; i++) {
+//   document.write(i + ", ");
+// }
+// document.write("<br><br>");
+
+
+// (b)
+
+// for (let i = 10; i >= 1; i--) {
+//   document.write(i + ", ");
+// }
+// document.write("<br><br>");
+
+// (c)
+
+// for (let i = 0; i <= 20; i += 2) {
+//   document.write(i + ", ");
+// }
+// document.write("<br><br>");
+
+
+// (d)
+
+
+// for (let i = 1; i <= 19; i += 2) {
+//   document.write(i + ", ");
+// }
+// document.write("<br><br>");
+
+
+
+// (e)
+
+
+// for (let i = 2; i <= 20; i += 2) {
+//   document.write(i + "k, ");
+// }
+
+// question 7
+
+
+// let items = ["cake", "apple pie", "cookie", "chips", "patties"];
+
+// let userInput = prompt("Enter item to search");
+
+// let found = false;
+
+// for (let i = 0; i <  items.length; i++) {
+
+//   if (userInput ===  items[i]) {
+//     found = true;
+//     alert(userInput + " is available in the list");
+//     break;
+//   }
+
+// }
+
+// if (found === false) {
+//   alert(userInput + " is not available in the list");
+// }
+
+
+
+
+
+// question 8
+
+
+// let large = [24, 53, 78, 91, 12];
+
+// let largest = large[0];
+
+// for (let i = 1; i < large.length; i++) {
+//   if (large[i] > largest) {
+//     largest = large[i];
+//   }
+// }
+
+// console.log(largest);
+
+
+
+// question 9
+
+
+// let small = [24, 53, 78, 91, 12];
+
+// let smallest  = small[0];
+
+// for (let i = 1; i < small.length; i++) {
+//   if (small[i] < smallest ) {
+//     smallest  = small[i];
+//   }
+// }
+
+// console.log(smallest );
+
+
+
+// question 10
+
+
+// for (let i = 5; i <= 100; i += 5) {
+//   console.log(i);
+// }
